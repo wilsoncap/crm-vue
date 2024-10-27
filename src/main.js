fb6e6240ebc70174ba76af6a20842302b4b9eaad
Wilson Capacho <wiston.98@hotmail.com>
2024-10-26 23:21:17 -0500

    installing formkit to create forms

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { plugin, defaultConfig } from '@formkit/vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(plugin, defaultConfig())
 
 app.mount('#app')
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { FormKit } from '@formkit/vue';
     import RouterLink from '@/components/UI/RouterLink.vue';
     import Heading from './Heading.vue';
 
@@ -19,6 +20,14 @@
         </div>
 
         <Heading>{{ titulo }}</Heading>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <FormKit
+                type="form"
+            >
+
+            </FormKit>
+        </div>
     </div>
 </template>
 
